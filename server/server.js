1cd7bebe2100d6c18aa14dc8b2297077388a92f9
Matt Schneeberger <helluvamatt@gmail.com>
2012-10-15 12:18:11 -0500

    Playing around with pub/sub (it doesn't work)

--- server/server.js
+++ server/server.js
@@ -2,38 +2,63 @@ var http = require('http');
 var sys = require('sys');
 var fs = require('fs');
 
+var sse = {};
+
 http.createServer(function(req, res) {
   debugHeaders(req);
 
+  var parms = require('url').parse(req.url, true);
+
+  console.log('Got some request params: \n\n %j \n\n', parms);
+
   if (req.headers.accept && req.headers.accept == 'text/event-stream') {
-    if (req.url == '/events') {
-      sendSSE(req, res);
+    if (parms.pathname == '/events' && parms.query.sessid) {
+      registerSSE(parms.query.sessid, res);
     } else {
       res.writeHead(404);
       res.end();
     }
   } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(fs.readFileSync(__dirname + '/sse-node.html'));
-    res.end();
+    if (parms.pathname == '/send') {
+      if (parms.query.sessid && parms.query.msg) {
+        if (sendMSG(parms.query.sessid, parms.query.msg)) {
+          res.writeHead(200, {'Content-type': 'text/plain'});
+          res.write('Success');
+          res.end();
+        } else {
+          res.writeHead(200, {'Content-type': 'text/plain'});
+          res.write('Failed');
+          res.end();
+        }
+      } else {
+        res.writeHead(404);
+        res.end();
+      }
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(fs.readFileSync(__dirname + '/sse-node.html'));
+      res.end();
+    }
   }
 }).listen(8000);
 
-function sendSSE(req, res) {
+function sendMSG(sessid, message) {
+  var res = sse.sessid;
+  if (!res) return false;
+  
+  constructSSE(res, sessid, message);
+
+  return true;
+}
+
+function registerSSE(sessid, res) {
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive'
   });
 
-  var id = (new Date()).toLocaleTimeString();
-
-  // Sends a SSE every 5 seconds on a single connection.
-  setInterval(function() {
-    constructSSE(res, id, (new Date()).toLocaleTimeString());
-  }, 5000);
-
-  constructSSE(res, id, (new Date()).toLocaleTimeString());
+  sse[sessid] = res;
 }
 
 function constructSSE(res, id, data) {
